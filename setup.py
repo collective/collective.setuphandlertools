0788d16b68466b6aa79c5ac28415089c5b282894
Johannes Raggam <johannes@raggam.co.at>
2010-05-17 17:26:10 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.setuphandlertools',
+      version=version,
+      description="Tools for setting up a Plone site.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone zope setup',
+      author='Johannes Raggam',
+      author_email='raggam-nl@adm.at',
+      url='http://github.com/thet/collective.setuphandlertools',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+      )
